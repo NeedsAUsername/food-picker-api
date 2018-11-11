ee401379914accd00f999ed56a17eab9bc3c36d9
NeedsAUsername <kevinchi118@gmail.com>
2018-11-11 15:46:26 -0500

    adds test routes and controller actions to see if json is being rendered

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,14 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user # method provided by Knock::Authenticable
+  # authenticate_user method provided by Knock::Authenticable
+  before_action :authenticate_user, except: [:test]
 
   def current
     render json: current_user # method provided by Knock
   end
 
+  # Just to see if the json is being rendered correctly
+  def test
+    @user = User.first
+    render json: @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get '/users/current' => 'users#current'
-    post '/users/token' => 'user_toekn#create'
+    get '/users/test' => 'users#test' #just a test route
+    post '/users/token' => 'user_token#create'
   end
 end

d1955ada87ee46a87f009660f3a9424a5a191c16
NeedsAUsername <kevinchi118@gmail.com>
2018-11-11 15:26:26 -0500

    adds belongs_to and has_many for user and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
-  validates :yelpNumber, presence: true 
+  validates :yelpNumber, presence: true
+  belongs_to :user 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
+  has_many :restaurants
 
   validates :username, :password, presence: true
   validates :username, uniqueness: true, length: {minimum: 4}

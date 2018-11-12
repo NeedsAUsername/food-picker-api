9fa17a423338b7c5568c89913602e90599e77e39
NeedsAUsername <kevinchi118@gmail.com>
2018-11-12 13:41:10 -0500

    adds devise 'able's to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :restaurants
 
-  validates :username, :password, presence: true
-  validates :username, uniqueness: true, length: {minimum: 4}
-  validates :password, length: {minimum: 8}
+  devise :database_authenticable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  # validates :username, :password, presence: true
+  # validates :username, uniqueness: true, length: {minimum: 4}
+  # validates :password, length: {minimum: 8}
 end

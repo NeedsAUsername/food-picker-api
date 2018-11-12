c1538a57640f25445a62d0e421855912e4cd4c0a
NeedsAUsername <kevinchi118@gmail.com>
2018-11-12 15:43:00 -0500

    writes session create action and route

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,7 +1,12 @@
 class V1::SessionsController < ApplicationController
 
-
   def create #login
+    @user = User.find_by(email: params[:email])
+    if @user.valid_password?(params[:password])
+      render json: @user
+    else
+      head(:unauthorized)
+    end
   end
 
   def destroy #logout
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
     get '/users/current' => 'users#current'
     get '/users/test' => 'users#test' #just a test route
+
+    post '/sessions' => 'sessions#create'
   end
 end

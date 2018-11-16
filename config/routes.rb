db230085901ffe588fb2a34173710f942f5ffd35
NeedsAUsername <kevinchi118@gmail.com>
2018-11-15 19:29:34 -0500

    sets up heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: ''
   namespace :v1 do
     get '/users/current' => 'users#current'
     get '/restaurants' => 'restaurants#index'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_13_010150) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "restaurants", force: :cascade do |t|
     t.string "yelpNumber"
     t.string "user_id"

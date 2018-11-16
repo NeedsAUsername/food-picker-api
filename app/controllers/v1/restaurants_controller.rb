50fd1a82404e3b62df8c5836a90e6196ceff2938
NeedsAUsername <kevinchi118@gmail.com>
2018-11-16 00:06:47 -0500

    removes uneeded action and route

--- app/controllers/v1/restaurants_controller.rb
+++ app/controllers/v1/restaurants_controller.rb
@@ -1,17 +1,5 @@
 class V1::RestaurantsController < ApplicationController
 
-  def index
-    # With the correct X-User-Email and X-User-Token in the header of the request, current_user will be set.
-    # Otherwise, current_user will be nil.
-    if current_user
-      @restaurants = current_user.restaurants
-      render json: @restaurants
-    else
-      # head(:unauthorized)   decided not to do this, because it's easier to just check the status in react
-      render json: {status: 'Not logged in'}
-    end
-  end
-
   def create
     if current_user
       if current_user.restaurants.find_by(yelpNumber: params[:restaurantId])
